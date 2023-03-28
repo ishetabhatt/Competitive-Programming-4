34338f8f853fcd09437198d9b23d6a2a35e80a5c
Isheta Bhatt <115787900+ishetabhatt@users.noreply.github.com>
2023-03-28 16:35:27 +0530

    Delete echoechoecho.java

--- C1 - Introduction/IO + Sequences Only/echoechoecho.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-class echoechoecho {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-        System.out.println(str + " " + str + " " + str);
-    }
-}
